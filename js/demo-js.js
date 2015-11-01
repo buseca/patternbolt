8c3a3d6ea56d1294f2650f8808f215e244598049
buseca <ruggero.motta@gmail.com>
2015-11-01 01:25:53 +0100

    js show and return home

--- js/demo-js.js
+++ js/demo-js.js
@@ -1,79 +1,77 @@
 $(document).ready(function(){
 
-
-
-	$(".item").click(function () {
-		$('.overlay').addClass('go');
-	});
-
-	$(".back").click(function () {
-		$('.overlay').removeClass('go');
-	});
-
-
-
-
-
-	$(".item").click(function () {
+var showUp = function(){
+	$(".showing-all .item").click(function () {
 		$('.selecta').removeClass('showing-all');
 		$('.item').addClass('reduce');
 		$(this).removeClass('reduce');
 		$(this).addClass('expand');
 	});
 
-
-
-
-
-
-
-
-
-
-	// cambio sfondo all'hover del tasto
-		$('.git').hover(function(){
-		  $('.demo').addClass('over');
-		  }, function(){
-		  $('.demo').removeClass('over');
-		  }
-		);
-
-
-	// controller del pattern
-	$(".tasto").click(function () {
-		var newPattern = $(this).attr('id');
-		var oldPattern = $('.tasto.active').attr('id');
-		$('.tasto.active').removeClass('active');
-		$(this).toggleClass("active");
-		// $('.demo').removeClass('iniziale');
-		$('.demo').removeClass('cross-light-thin');
-		$('.demo').removeClass(oldPattern);
-		$('.demo').addClass(newPattern);
-		$('#classe-pb span').html( newPattern );
+	$(".expand").click(function () {
+		$('.selecta').addClass('showing-all');
+		$('.item').removeClass('reduce');
+		$(this).removeClass('expand');
 	});
-
-	// controller della dimensione
-	$("#size-pattern").change(function () {
-		var pbSize = $(this).val();
-		$('.demo').removeClass( function (index, css) { return (css.match(/\bpb-size-\S+/g) || []).join(' '); });
-		$('.demo').addClass('pb-size-' + pbSize);
-		$('#bg-size-pb').html('.ted:after { background-size: ' + pbSize + 'px ;}')
-	});
-
-	// controller del colore
-	$("#color-pattern").change(function () {
-		var pbColor = $(this).val();
-		$('.demo').css('background-color',pbColor)
-		$('#bg-color-pb').html('.ted { background-color: ' + pbColor + ' ;}')
-	});
+}
 
 
-	// showcase
-	$('.showcase .pb-pattern').hover(function(){
-	  $(this).addClass('active');
-	  }, function(){
-	  $(this).removeClass('active');
-	  }
-	);
+setInterval(function() {
+	showUp();
+}, 500);
+
+
+
+
+
+
+
+// // js vecchio_____________________________________
+
+// 	// cambio sfondo all'hover del tasto
+// 		$('.git').hover(function(){
+// 		  $('.demo').addClass('over');
+// 		  }, function(){
+// 		  $('.demo').removeClass('over');
+// 		  }
+// 		);
+
+
+// 	// controller del pattern
+// 	$(".tasto").click(function () {
+// 		var newPattern = $(this).attr('id');
+// 		var oldPattern = $('.tasto.active').attr('id');
+// 		$('.tasto.active').removeClass('active');
+// 		$(this).toggleClass("active");
+// 		// $('.demo').removeClass('iniziale');
+// 		$('.demo').removeClass('cross-light-thin');
+// 		$('.demo').removeClass(oldPattern);
+// 		$('.demo').addClass(newPattern);
+// 		$('#classe-pb span').html( newPattern );
+// 	});
+
+// 	// controller della dimensione
+// 	$("#size-pattern").change(function () {
+// 		var pbSize = $(this).val();
+// 		$('.demo').removeClass( function (index, css) { return (css.match(/\bpb-size-\S+/g) || []).join(' '); });
+// 		$('.demo').addClass('pb-size-' + pbSize);
+// 		$('#bg-size-pb').html('.ted:after { background-size: ' + pbSize + 'px ;}')
+// 	});
+
+// 	// controller del colore
+// 	$("#color-pattern").change(function () {
+// 		var pbColor = $(this).val();
+// 		$('.demo').css('background-color',pbColor)
+// 		$('#bg-color-pb').html('.ted { background-color: ' + pbColor + ' ;}')
+// 	});
+
+
+// 	// showcase
+// 	$('.showcase .pb-pattern').hover(function(){
+// 	  $(this).addClass('active');
+// 	  }, function(){
+// 	  $(this).removeClass('active');
+// 	  }
+// 	);
 
 });
