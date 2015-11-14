d82ad8e19333c0f7a04fab7dfb1b06b3bfbd7f99
buseca <ruggero.motta@gmail.com>
2015-11-14 22:54:36 +0100

    new page ready
    - added content and links
    - patterns renamed

--- js/demo-js.js
+++ js/demo-js.js
@@ -5,20 +5,27 @@ var showUp = function(){
 		$('.selecta').removeClass('showing-all');
 		$('.item').addClass('reduce');
 		$(this).removeClass('reduce');
-		$(this).addClass('expand medium');
+		$(this).addClass('expanded medium');
 		$('.controllers').addClass('showit');
+
+		$('.content').addClass('hidden');
 	});
 
-	$(".expand").click(function () {
+	$(".expanded").click(function () {
 		$('.selecta').addClass('showing-all');
 		$('.item').removeClass('reduce');
-		$(this).removeClass('expand');
+		$(this).removeClass('expanded');
 		$('.controllers').removeClass('showit');
+
+		$('.content').removeClass('hidden');
 	});
 
 }
 
 $(".zoomin").click(function () {
+	$('.xxlarge').addClass('xxxlarge');
+	$('.xxlarge').removeClass('xxlarge');
+
 	$('.xlarge').addClass('xxlarge');
 	$('.xlarge').removeClass('xlarge');
 
@@ -51,6 +58,9 @@ $(".zoomout").click(function () {
 	$('.xxlarge').addClass('xlarge');
 	$('.xxlarge').removeClass('xxlarge');
 
+	$('.xxxlarge').addClass('xxlarge');
+	$('.xxxlarge').removeClass('xxxlarge');
+
 });
 
 
@@ -59,57 +69,5 @@ setInterval(function() {
 }, 500);
 
 
-
-
-
-
-
-// // js vecchio_____________________________________
-
-// 	// cambio sfondo all'hover del tasto
-// 		$('.git').hover(function(){
-// 		  $('.demo').addClass('over');
-// 		  }, function(){
-// 		  $('.demo').removeClass('over');
-// 		  }
-// 		);
-
-
-// 	// controller del pattern
-// 	$(".tasto").click(function () {
-// 		var newPattern = $(this).attr('id');
-// 		var oldPattern = $('.tasto.active').attr('id');
-// 		$('.tasto.active').removeClass('active');
-// 		$(this).toggleClass("active");
-// 		// $('.demo').removeClass('iniziale');
-// 		$('.demo').removeClass('cross-light-thin');
-// 		$('.demo').removeClass(oldPattern);
-// 		$('.demo').addClass(newPattern);
-// 		$('#classe-pb span').html( newPattern );
-// 	});
-
-// 	// controller della dimensione
-// 	$("#size-pattern").change(function () {
-// 		var pbSize = $(this).val();
-// 		$('.demo').removeClass( function (index, css) { return (css.match(/\bpb-size-\S+/g) || []).join(' '); });
-// 		$('.demo').addClass('pb-size-' + pbSize);
-// 		$('#bg-size-pb').html('.ted:after { background-size: ' + pbSize + 'px ;}')
-// 	});
-
-// 	// controller del colore
-// 	$("#color-pattern").change(function () {
-// 		var pbColor = $(this).val();
-// 		$('.demo').css('background-color',pbColor)
-// 		$('#bg-color-pb').html('.ted { background-color: ' + pbColor + ' ;}')
-// 	});
-
-
-// 	// showcase
-// 	$('.showcase .pb-pattern').hover(function(){
-// 	  $(this).addClass('active');
-// 	  }, function(){
-// 	  $(this).removeClass('active');
-// 	  }
-// 	);
 
 });
